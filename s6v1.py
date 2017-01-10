863da4c5e296ed90d6b1ee6afc181a9a1d059954
Ken <kpjohn3@uky.edu>
2017-01-10 23:13:41 +0000

    initial commit

---
+++ s6v1.py
@@ -0,0 +1,18 @@
+from s5v3.py import *
+
+def create_line_chart(data_sample, title, exported_figure_filename):
+  fig = plt.figure()
+  ax = fig.add_subplot(1, 1, 1)
+  
+  prices = (sorted(map(float, data_sample)))
+  
+  x_axis_ticks = list(range(len(data_sample)))
+  ax.plot(x_axis_ticks, prices , linewidth=2)
+  ax.set_title(title)
+  ax.set_xlim([0, len(data_sample)])
+  ax.set_xlabel('THIS IS THE X')
+  ax.set_ylabel('THIS IS THE Y')
+  
+  fig.savefig(exported_figure_filename)
+  
+create_line_chart([float(x[2]) for x in jcrew_ties], "THIS IS THE TITLE, "labels.png")
